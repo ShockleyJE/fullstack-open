416f146733303788ff0c0262b84c33297b6a5edb
shockleyje <8533782+ShockleyJE@users.noreply.github.com>
2022-08-11 15:00:03 -0400

    Exercises 2.1-2.4

---
+++ part2/courseinfo/src/App.js
@@ -0,0 +1,90 @@
+const Header = ({ course }) => <h1>{course}</h1>;
+
+const Total = ({ parts }) => (
+  <p>
+    Number of exercises {parts.reduce((prev, curr) => prev + curr.exercises, 0)}
+  </p>
+);
+
+const Part = ({ part }) => (
+  <p>
+    {part.name} {part.exercises}
+  </p>
+);
+
+const Content = ({ parts }) => (
+  <>
+    {parts.map((part) => {
+      return <Part key={part.id} part={part} />;
+    })}
+  </>
+);
+
+const Courses = ({ courses }) => (
+  <>
+    {courses.map((course) => (
+      <Course key={course.id} course={course} />
+    ))}
+  </>
+);
+
+const Course = ({ course }) => {
+  console.log(course);
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  );
+};
+
+const App = () => {
+  const courses = [
+    {
+      name: "Half Stack application development",
+      id: 1,
+      parts: [
+        {
+          name: "Fundamentals of React",
+          exercises: 10,
+          id: 1,
+        },
+        {
+          name: "Using props to pass data",
+          exercises: 7,
+          id: 2,
+        },
+        {
+          name: "State of a component",
+          exercises: 14,
+          id: 3,
+        },
+        {
+          name: "Redux",
+          exercises: 11,
+          id: 4,
+        },
+      ],
+    },
+    {
+      name: "Node.js",
+      id: 2,
+      parts: [
+        {
+          name: "Routing",
+          exercises: 3,
+          id: 1,
+        },
+        {
+          name: "Middlewares",
+          exercises: 7,
+          id: 2,
+        },
+      ],
+    },
+  ];
+
+  return <Courses courses={courses} />;
+};
+export default App;
